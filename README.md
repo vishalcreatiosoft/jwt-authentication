99e2cf66e41e12dc9481ef7e8c340c1ff0bcfd44
vishalcreatiosoft <vishal.srivastava@creatiosoft.com>
2022-06-03 14:00:11 +0530

    ssl implementation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # jwt-authentication
+
+ ## This code is tutorial for learning 
+   - How to implement jwt-auth in node.
+   - How to implement ssl in node.
--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 const express = require('express');
+require('dotenv').config();
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 
 
-app.listen(port,()=>{
-    console.log(`server started on port ${port}`);
-})
+
+
+
+
+
+
+if(process.env.ENABLE_SSL === 'true'){
+    
+    /******  create a folder 'cert' inside put your privateKey and certificate files and add the filepath accordingly *******/ 
+
+    const privateKey = fs.readFileSync("filePath","utf-8");
+    const certificate = fs.readFileSync("filePath","utf-8");
+    const credentials = {key : privateKey, cert : certificate};
+
+    https.createServer(credentials,app).listen(port, ()=>{
+        console.log(`server started on port ${port}`);
+    });
+}
+else{
+
+    http.createServer(app).listen(port, ()=>{
+        console.log(`server started on port ${port}`);
+    })
+}
+
 
