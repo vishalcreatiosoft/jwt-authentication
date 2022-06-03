ccfb551d57cf696dc12075b42d72ca7a5a557d68
vishalcreatiosoft <vishal.srivastava@creatiosoft.com>
2022-06-03 13:38:35 +0530

    iniital code

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+
+app.listen(port,()=>{
+    console.log(`server started on port ${port}`);
+})
+
